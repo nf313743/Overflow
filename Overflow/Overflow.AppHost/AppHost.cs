2c4dbda3c921c22e5f7eae0832fcabfd03b8139e
nf313743 <1612339+nf313743@users.noreply.github.com>
2026-03-12 21:31:49 +0000

    Section 10 completed

--- Overflow/Overflow.AppHost/AppHost.cs
+++ Overflow/Overflow.AppHost/AppHost.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var keycloak = builder.AddKeycloak("keycloak", 6001)
+    .WithDataVolume("keycloack-data");
+
+
+
 builder.Build().Run();
